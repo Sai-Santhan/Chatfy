ba0e0b1219f415df8d77c4fc061bc1fa77dee041
procrastinator333 <dsaisanthan@gmail.com>
2021-06-23 15:25:25 +0530

    created an APIView to create Room

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
